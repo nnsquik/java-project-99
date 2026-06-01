36d0173bdec9a630dbced6b1e38e535ddfc2390d
nnsquik <nnsquik@outlook.com>
2026-06-01 17:45:57 +0300

    update rsa keyes for tests

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -2,8 +2,10 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AppApplicationTests {
     @Test
     void contextLoads() {
