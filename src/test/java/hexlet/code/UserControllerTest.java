2b463dff1f157735dfe5dd07bc53cfefe1957099
nnsquik <nnsquik@outlook.com>
2026-06-01 17:35:33 +0300

    add rsa keyes for tests

--- src/test/java/hexlet/code/UserControllerTest.java
+++ src/test/java/hexlet/code/UserControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @SpringBootTest
-
+@ActiveProfiles("test")
 public class UserControllerTest {
 
     @Autowired
